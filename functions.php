d8e8c0c2568193c84c5b7056e61228e6acdb6846
T. Thomas <TdotThomas@SpaceAgeMinds.com>
2020-09-16 00:05:29 -0700

    1. Removed random endpoint code from the theme. The function will be added back in via a plugin.

--- functions.php
+++ functions.php
@@ -12,44 +12,6 @@ function my_theme_enqueue_styles() {
     );
 }
 
-//Start Custom Code for a Random Endpoints
-function random_endpoint() {
-	    add_rewrite_endpoint( 'random', EP_ROOT );
-}
-add_action( 'init', 'random_endpoint' );
-function random_redirect() {
-// If we have accessed our /random/ endpoints.
-$post_type = get_query_var( 'random' );
-if ( $post_type == '' ) {
-	$post_type = [ 'post', 'page', 'product' ];
-}
-$query_string = "";
-$arr = explode("?",$_SERVER['REQUEST_URI']);
-if (count($arr) == 2){
-	$query_string = "?".end($arr);
-}
-if ( get_query_var( 'random', false ) !== false ) {
-	// Get a random post.
-	$random_post = get_posts( [
-            'numberposts' => 1,
-            'post_type'   => $post_type,
-            'orderby'     => 'rand',
-        ] );
-        // If we found one.
-        if ( ! empty( $random_post ) ) {
-            // Get its URL.
-            $url = esc_url_raw( get_the_permalink( $random_post[0] ) );
-            // Escape it.
-            $url = esc_url_raw($url.$query_string);
-            // Redirect to it.
-            wp_safe_redirect( $url, 302 );
-            exit;
-        }
-    }
-}
-add_action( 'template_redirect', 'random_redirect' );
-//End Custom Code for a Random Endpoints
-
 //Start Custom Code for Random Tag Clouds
 add_filter( 'tag_cloud_sort', 'shuffle_tags', 10, 2 );
 function shuffle_tags( $tags, $args ) {
